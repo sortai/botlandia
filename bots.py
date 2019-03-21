92f0475df39ef81f5aee5e8a0288c4a16b686b3f
sortai <32301806+sortai@users.noreply.github.com>
2019-03-21 15:59:16 +0100

    Update bots.py

--- bots.py
+++ bots.py
@@ -1,5 +1,13 @@
 #!/bin/python3
 
+defstats = {
+    "inp": 30.,
+    "nip": .1,
+    "dir": 0.
+    }
+
 class bot:
-    def __init__(self, name):
+    def __init__(self, name, stats = None, affinities = None):
         self.name = name
+        if stats is None: stats = dict(defstats)
+        if affinites is None: affinities = dict()
